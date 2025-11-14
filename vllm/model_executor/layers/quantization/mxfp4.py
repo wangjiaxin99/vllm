b05a442675caa41446697bee263911e5eeed52ac
Rohan138 <rohanpotdar138@gmail.com>
2025-11-14 00:37:33 -0600

    Add weight view for e2m1, cleanup

--- vllm/model_executor/layers/quantization/mxfp4.py
+++ vllm/model_executor/layers/quantization/mxfp4.py
@@ -498,13 +498,16 @@ class Mxfp4MoEMethod(FusedMoEMethodBase):
                 e, n, k = w13_aiter_weight.shape
                 w13_aiter_weight = w13_aiter_weight.view(e, n // 2, 2, k).permute(0, 2, 1, 3).contiguous().view(e, n, k)
                 w13_aiter_scale = w13_aiter_scale.view(e, n // 2, 2, -1).permute(0, 2, 1, 3).contiguous().view(e, n, -1)
+
+                w13_aiter_weight = w13_aiter_weight.view(torch.float4_e2m1fn_x2)
+                w13_aiter_scale = w13_aiter_scale.view(-1, w13_aiter_scale.shape[-1])
+                w2_aiter_weight = w2_aiter_weight.view(torch.float4_e2m1fn_x2)
+                w2_aiter_scale = w2_aiter_scale.view(-1, w2_aiter_scale.shape[-1])
                 
                 self.w13_weight_aiter_tensor = shuffle_weight_a16w4(w13_aiter_weight, 16, True)
-                w13_aiter_scale = w13_aiter_scale.view(-1, k // 16)
-                self.w13_scale_aiter_tensor = shuffle_scale_a16w4(w13_aiter_scale, e, True)
+                self.w13_scale_aiter_tensor = shuffle_scale_a16w4(w13_aiter_scale, self.num_experts, True)
                 self.w2_weight_aiter_tensor = shuffle_weight_a16w4(w2_aiter_weight, 16, False)
-                w2_aiter_scale = w2_aiter_scale.view(-1, k // 16)
-                self.w2_scale_aiter_tensor = shuffle_scale_a16w4(w2_aiter_scale, e, False)
+                self.w2_scale_aiter_tensor = shuffle_scale_a16w4(w2_aiter_scale, self.num_experts, False)
                 self.w13_bias_aiter_tensor = layer.w13_bias.view(-1, n // 2, 2).permute(0, 2, 1).contiguous().view(-1, n)
             else: 
                 w13_weight, w13_flex, w13_scale = _swizzle_mxfp4(
